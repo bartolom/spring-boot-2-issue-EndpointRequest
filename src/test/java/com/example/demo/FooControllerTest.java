ba528a7f7d34e1a8ee5b7c3c1962e3583e483219
bartolom <bart@nice-person.net>
2018-02-27 00:03:24 +0100

    demo project to show case issue

---
+++ src/test/java/com/example/demo/FooControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(FooController.class)
+public class FooControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	@WithMockUser(username = "staff", roles = { "STAFF" })
+	public void testBar() throws Exception {
+		mvc.perform(get("/bar")
+				.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
+	}
+	
+}
